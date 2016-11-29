26bd05cf95339ae0ae6853c0fe5f628d312c23c6
lewischa <lewischa@sonoma.edu>
2016-11-28 19:28:06 -0800

    Final Commit. A5 calculation and user-input functions have been implemented successfully.

--- problem4.py
+++ problem4.py
@@ -9,9 +9,27 @@ class Node:
 		self.edges = []
 		self.visited = False
 		self.num_edges = 0
+		self.expectedA5 = 0
+		self.A5isDone = False
 		self.populateEdges()
 
-	def isLastEdge(self):
+	def isLastEdge(self, edge):
+		numEdges = len(self.edges)
+		if numEdges > 1:
+			if self.edges[numEdges - 1] == edge:
+				return True
+			else:
+				return False
+		else:
+			key_digits = [int(char) for char in self.key]
+			lastDigit = len(key_digits) - 1
+			if key_digits[lastDigit] != 0:
+				return True
+			else:
+				return False
+
+
+	def isFinalEdge(self):
 		if self.key == '0001': return True
 		else: return False
 		# key_digits = [int(char) for char in self.key]
@@ -45,7 +63,7 @@ class Node:
 		return [edge, edge_weight]
 
 	def populateEdges(self):
-		if self.isLastEdge(): return
+		if self.isFinalEdge(): return
 		for i, c in enumerate(self.key):
 			if self.key[i] != "0":
 				self.edges.append(self.generateEdge(i))
@@ -147,6 +165,8 @@ class AdjacencyList:
 
 # Builds a graph (adjacency list) from given 'key'
 # Topologically sorts the graph and stores it in 'sorted[]'
+# Provides the ability to calculate the number of A5's one
+	# is expected to draw starting with 1 of each size sheet
 class Graph:
 	def __init__(self, key):
 		self.adjListObject = AdjacencyList(key)
@@ -203,9 +223,115 @@ class Graph:
 				self.explore(node)
 		self.fillSorted()
 
+	# Recursively and iteratively calculate expected number of A5's
+	def _calc_expectedA5(self, key):
+		node = self.adjListObject.find(key)
+		numEdges = len(node.edges)
+
+		# Recursive 'break' condition:
+			# Return if you're at the very last node ('0001')
+		if numEdges == 0:
+			self.adjListObject.find(key).expectedA5 = 0
+			self.adjListObject.find(key).A5isDone = True
+			return 0
+		tempTotal = 0
+
+		# Iterate through edges of every Node, recursively calculating
+			# expected A5's for each node that Node points to
+		for edge in node.edges:
+			tempNode = self.adjListObject.find(edge[0])
+
+			# If we've already calculated A5's for the node in edge[0], just use that instead
+				# of making another recursive call
+			if tempNode.A5isDone:
+				if node.isLastEdge(edge):
+					self.adjListObject.find(key).expectedA5 += (0 + tempNode.expectedA5) * edge[1]
+				else:
+					self.adjListObject.find(key).expectedA5 += tempNode.expectedA5 * edge[1]
+
+			# Check if we need to add 1 to the calculation (if the edge corresponds to
+				# pulling an A5)
+			elif node.isLastEdge(edge):
+				self.adjListObject.find(key).expectedA5 += (1 + self._calc_expectedA5(edge[0])) * edge[1]
+			else:
+				self.adjListObject.find(key).expectedA5 += self._calc_expectedA5(edge[0]) * edge[1]
+
+		# We've iterated through each of Node's edges after the for loop,
+			# so we are done calculating Node's expected A5. Set 'A5isDone' to True
+			# and return expected A5.
+		self.adjListObject.find(key).A5isDone = True
+		return self.adjListObject.find(key).expectedA5
+
+	# Top-level handler to call internal function w/ proper key
+	# to calculate expected number of A5's
+	def calc_expectedA5(self):
+		tempNode = self.adjListObject.find('1111')
+		if tempNode.A5isDone:
+			return tempNode.expectedA5
+		return self._calc_expectedA5('1111')
+##########################    END CLASSES    ######################################
 
 
-x = Graph('1111')
-x.adjListObject.printList()
-# print(x.sorted)
+
+# Print list of options that user can enter
+def printOptions():
+	print(" ")
+	print("***  OPTIONS  ***")
+	print("1. Print adjacency list")
+	print("2. Print topologically sorted dag")
+	print("3. Calculate and print expected number of A5\'s")
+	print(" ")
+
+# Ask user for specific option (invalid input will be handled)
+def gatherInput():
+	printOptions()
+	option = input("Enter your option (1, 2, or 3): ")
+	while (option != '1') and (option != '2') and (option != '3'):
+		print("That is not a valid option. Only 1, 2, or 3 are accepted.")
+		option = input("Try again: ")
+	return option
+
+# Do the actual option that user selected (invalid option will be handled)
+def runOption(option, graph):
+	if option == '1':
+		print("Adjacency List: ")
+		graph.adjListObject.printList()
+		return
+	elif option == '2':
+		print("\nTopologically Sorted Graph: \n")
+		print(graph.sorted)
+		return
+	elif option == '3':
+		print("\nExpected Number of A5\'s: \n")
+		print(graph.calc_expectedA5())
+		return
+	else:
+		print("There was an error with the option selected.\n")
+		return
+
+def main():
+	print("\nCreating directed, acyclic graph beginning with node \'1111\'. . .")
+	dag = Graph('1111')
+	print("The graph has been created.\n")
+	option = gatherInput()
+	runOption(option, dag)
+	again = input("\nWould you like to choose a different option (y/n)? ")
+	again = again.lower()
+	while again != 'n':
+		while again != 'y':
+			again = input("I didn't recognize that. Would you like to continue (y/n)? ")
+			again = again.lower()
+			if again == 'n':
+				print("All done. Exiting . . .")
+				exit()
+		option = gatherInput()
+		runOption(option, dag)
+		again = input("\nWould you like to choose a different option (y/n)? ")
+		again = again.lower()
+	print("All done. Exiting . . .")
+	exit()
+
+
+if __name__ == "__main__":
+	main()
 
